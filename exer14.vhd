4b94a780ec9042efa4bf9344cc240006f932e651
Sampie159 <sfamboni@gmail.com>
2020-11-29 16:42:28 -0300

    Exer14 Completo

---
+++ exer14.vhd
@@ -0,0 +1,61 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity portas is
+port(
+    A, B, Cin: in std_logic;
+    S, Cout: out std_logic
+);
+end portas;
+
+architecture somador of portas is
+begin
+    process(A, B, Cin) is
+    begin
+        S <= '1' when (A = '1' and B = '1' and Cin = '1') else
+             '1' when (A = '1' and B = '0' and Cin = '0') else
+             '1' when (A = '0' and B = '1' and Cin = '0') else
+             '1' when (A = '0' and B = '0' and Cin = '1') else '0';
+        Cout <= '1' when (A = '1' and B = '1' and Cin = '1') else
+                '1' when (A = '1' and B = '1' and Cin = '0') else
+                '1' when (A = '1' and B = '0' and Cin = '1') else
+                '1' when (A = '0' and B = '1' and Cin = '1') else '0';
+        -- if A = '0' then
+        --     if B = '0' then
+        --         if Cin = '0' then
+        --             S <= '0';
+        --             Cout <= '0';
+        --         else
+        --             S <= '1';
+        --             Cout <= '0';
+        --         end if;
+        --     else
+        --         if Cin = '0' then
+        --             S <= '1';
+        --             Cout <= '0';
+        --         else
+        --             S <= '0';
+        --             Cout <= '1';
+        --         end if;
+        --     end if;
+        -- else
+        --     if B = '0' then
+        --         if Cin = '0' then
+        --             S <= '1';
+        --             Cout <= '0';
+        --         else
+        --             S <= '0';
+        --             Cout <= '1';
+        --         end if;
+        --     else
+        --         if Cin = '0' then
+        --             S <= '0';
+        --             Cout <= '1';
+        --         else
+        --             S <= '1';
+        --             Cout <= '1';
+        --         end if;
+        --     end if;
+        -- end if;
+    end process;
+end somador ; -- somador
---
+++ testbenchexer14.vhd
@@ -0,0 +1,78 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity test is
+end test;
+
+architecture teste of test is
+component portas is
+port(
+    A, B, Cin: in std_logic;
+    S, Cout: out std_logic
+);
+end component;
+
+signal AT, BT, CinT, ST, CoutT: std_logic;
+
+begin
+    DUT: portas port map(AT, BT, CinT, ST, CoutT);
+
+    process
+    begin
+        AT <= '0';
+        BT <= '0';
+        CinT <= '0';
+        wait for 1 ns;
+        assert(ST = '0') report "Falha para S em 0/0/0" severity error;
+        assert(CoutT = '0') report "Falha para Cout em 0/0/0" severity error;
+        
+        AT <= '0';
+        BT <= '0';
+        CinT <= '1';
+        wait for 1 ns;
+        assert(ST = '1') report "Falha para S em 0/0/1" severity error;
+        assert(CoutT = '0') report "Falha para Cout em 0/0/1" severity error;
+        
+        AT <= '0';
+        BT <= '1';
+        CinT <= '0';
+        wait for 1 ns;
+        assert(ST = '1') report "Falha para S em 0/1/0" severity error;
+        assert(CoutT = '0') report "Falha para Cout em 0/0/1" severity error;
+        
+        AT <= '1';
+        BT <= '0';
+        CinT <= '0';
+        wait for 1 ns;
+        assert(ST = '1') report "Falha para S em 1/0/0" severity error;
+        assert(CoutT = '0') report "Falha para Cout em 1/0/0" severity error;
+        
+        AT <= '1';
+        BT <= '0';
+        CinT <= '1';
+        wait for 1 ns;
+        assert(ST = '0') report "Falha para S em 1/0/1" severity error;
+        assert(CoutT = '1') report "Falha para Cout em 1/0/1" severity error;
+        
+        AT <= '1';
+        BT <= '1';
+        CinT <= '0';
+        wait for 1 ns;
+        assert(ST = '0') report "Falha para S em 1/1/0" severity error;
+        assert(CoutT = '1') report "Falha para Cout em 1/1/0" severity error;
+        
+        AT <= '1';
+        BT <= '1';
+        CinT <= '1';
+        wait for 1 ns;
+        assert(ST = '1') report "Falha para S em 1/1/1" severity error;
+        assert(CoutT = '1') report "Falha para Cout em 1/1/1" severity error;
+        
+        AT <= '0';
+        BT <= '0';
+        CinT <= '0';
+        wait;
+        assert false report "Teste completo." severity note;
+    end process;
+
+end teste;
